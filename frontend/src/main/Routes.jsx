a71563e023b9070243ddb5f67d26fdfc7a73eace
Daniel dos Santos Araujo <denenewton@icloud.com>
2021-01-23 18:15:23 -0300

    add

--- frontend/src/main/Routes.jsx
+++ frontend/src/main/Routes.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import { Switch, Route, Redirect } from 'react-router'
 
 import Home from '../components/home/Home'
-import UserCrud from '../components/user/UserCrud'
+import User from '../components/user/User'
 
 export default props => 
     <Switch>
         <Route exact path='/' component={Home} />
-        <Route path='/users' component={UserCrud} />
+        <Route path='/users' component={User} />
         <Redirect from='*' to='/' />
     </Switch>
