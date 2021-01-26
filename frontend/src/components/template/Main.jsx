95e6611a5030c476821b45eb73b47504d1d8f949
Daniel dos Santos Araujo <denenewton@icloud.com>
2021-01-26 16:09:13 -0300

    alterações na tag main.

--- frontend/src/components/template/Main.jsx
+++ frontend/src/components/template/Main.jsx
@@ -6,7 +6,7 @@ export default props =>
     <React.Fragment>
         <Header {...props} />
         <main className="content container-fluid">
-            <div className="p-3 mt-3">
+            <div className="p-4 mt-3">
                 {props.children}
             </div>
         </main>
